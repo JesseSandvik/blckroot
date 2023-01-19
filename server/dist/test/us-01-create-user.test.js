ec8c1e0837709ad35dc1bab378933600af500354
Jesse <sandvikjesse@gmail.com>
2023-01-18 22:05:14 -0500

    US-01: added 400 error tests for missing data

--- server/dist/test/us-01-create-user.test.js
+++ server/dist/test/us-01-create-user.test.js
@@ -16,9 +16,34 @@ const globals_1 = require("@jest/globals");
 const supertest_1 = __importDefault(require("supertest"));
 const app_1 = __importDefault(require("../app"));
 (0, globals_1.describe)("US-01. Create A New User", () => {
-    (0, globals_1.test)("Returns 201 status for POST request", () => __awaiter(void 0, void 0, void 0, function* () {
-        const username = "eforeman70";
-        const password = "ILoveDonn@4ever";
+    const username = "eforeman70";
+    const password = "ILoveDonn@4ever";
+    (0, globals_1.test)("Returns 400 status if data is missing", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({});
+        (0, globals_1.expect)(response.status).toEqual(400);
+        (0, globals_1.expect)(response.body.error).toBeDefined();
+    }));
+    (0, globals_1.test)("Returns 400 status if username is missing", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({ password: password });
+        (0, globals_1.expect)(response.status).toEqual(400);
+        (0, globals_1.expect)(response.body.error).toBeDefined();
+    }));
+    (0, globals_1.test)("Returns 400 status if username is empty", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({ username: "", password: password });
+        (0, globals_1.expect)(response.status).toEqual(400);
+        (0, globals_1.expect)(response.body.error).toBeDefined();
+    }));
+    (0, globals_1.test)("Returns 400 status if password is missing", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({ username: username });
+        (0, globals_1.expect)(response.status).toEqual(400);
+        (0, globals_1.expect)(response.body.error).toBeDefined();
+    }));
+    (0, globals_1.test)("Returns 400 status if password is empty", () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({ username: username, password: "" });
+        (0, globals_1.expect)(response.status).toEqual(400);
+        (0, globals_1.expect)(response.body.error).toBeDefined();
+    }));
+    (0, globals_1.test)("Returns 201 status if POST request successful", () => __awaiter(void 0, void 0, void 0, function* () {
         const response = yield (0, supertest_1.default)(app_1.default).post("/register").send({ username: username, password: password });
         (0, globals_1.expect)(response.status).toEqual(201);
         (0, globals_1.expect)(response.body.error).toBeUndefined();
--- server/src/test/us-01-create-user.test.ts
+++ server/src/test/us-01-create-user.test.ts
@@ -3,10 +3,40 @@ import request from "supertest";
 import app from "../app";
 
 describe("US-01. Create A New User", () => {
-    test("Returns 201 status for POST request", async () => {
-        const username="eforeman70";
-        const password="ILoveDonn@4ever";
-        
+    const username="eforeman70";
+    const password="ILoveDonn@4ever";
+
+    test("Returns 400 status if data is missing", async () => {
+        const response = await request(app).post("/register").send({});
+
+        expect(response.status).toEqual(400);
+        expect(response.body.error).toBeDefined();
+    });
+    test("Returns 400 status if username is missing", async () => {
+        const response = await request(app).post("/register").send({ password: password });
+
+        expect(response.status).toEqual(400);
+        expect(response.body.error).toBeDefined();
+    });
+    test("Returns 400 status if username is empty", async () => {
+        const response = await request(app).post("/register").send({ username: "", password: password });
+
+        expect(response.status).toEqual(400);
+        expect(response.body.error).toBeDefined();
+    });
+    test("Returns 400 status if password is missing", async () => {
+        const response = await request(app).post("/register").send({ username: username });
+
+        expect(response.status).toEqual(400);
+        expect(response.body.error).toBeDefined();
+    });
+    test("Returns 400 status if password is empty", async () => {
+        const response = await request(app).post("/register").send({ username: username, password: "" });
+
+        expect(response.status).toEqual(400);
+        expect(response.body.error).toBeDefined();
+    });
+    test("Returns 201 status if POST request successful", async () => {
         const response = await request(app).post("/register").send({ username: username, password: password });
 
         expect(response.status).toEqual(201);
