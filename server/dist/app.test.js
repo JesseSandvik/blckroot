a23c74bcf0d5399934cfc63b264b20ed394afde5
Jesse <sandvikjesse@gmail.com>
2023-01-22 18:22:23 -0500

    Jest configured, smoke test created and confirmed operating as expected

---
+++ server/dist/app.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe("Smoke Test", () => {
+    test("Smoke Test", () => {
+        const smokeTest = "Hello World!";
+        expect(smokeTest).toEqual("Hello World!");
+    });
+});
---
+++ server/src/app.test.ts
@@ -0,0 +1,8 @@
+
+describe("Smoke Test", () => {
+    test("Smoke Test", () => {
+        const smokeTest = "Hello World!";
+
+        expect(smokeTest).toEqual("Hello World!");
+    });
+});
