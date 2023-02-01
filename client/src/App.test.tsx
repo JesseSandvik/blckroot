3e60be7fbb205d392e8a5e0f7bda7c8652089e09
Jesse <sandvikjesse@gmail.com>
2023-02-01 12:35:33 -0500

    Client: US-01 wrote success scenario test, test currently failing

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe("US-01: Create A New User", () => {
-  test("Inputting valid create new user form data and successfully creating new user routes user to dashboard", () => {});
-  test("Inputting invalid create new user 'username' form data and clicking 'Sign Up' button displays an error", () => {});
+  test("Inputting valid create new user form data and successfully creating new user routes user to dashboard", async () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    userEvent.click(await screen.findByRole("button", { name: /sign up/i }));
+
+    userEvent.type(await screen.findByLabelText(/username/i), "test_username123");
+    userEvent.type(await screen.findByLabelText(/password/i), "psswrd123@");
+    userEvent.type(await screen.findByLabelText(/password match/i), "psswrd123@");
+
+    userEvent.click(await screen.findByRole("button", { name: /sign up/i }));
+
+    const currentPageHeading = await screen.findByRole("heading", { name: /dashboard/i });
+
+    expect(currentPageHeading).toBeInTheDocument();
+  });
+  test("Inputting invalid create new user 'username' form data and clicking 'Sign Up' button displays an error", async () => {});
   test("Inputting invalid create new user 'password' form data and clicking 'Sign Up' button displays an error", () => {});
   test("Inputting invalid create new user 'password match' form data and clicking 'Sign Up' button displays an error", () => {});
 });
