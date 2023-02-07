2d3dba6fc53388ba06f21f8a49b7944696ec558c
Jesse <sandvikjesse@gmail.com>
2023-02-06 21:33:36 -0500

    Client: US-01, username, password, match password submit functioning, currently printing to console

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -3,15 +3,27 @@ import React, { useState } from 'react';
 function SignUpPage() {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [passwordMatch, setPasswordMatch] = useState<string>();
+    const [passwordMatch, setPasswordMatch] = useState<string>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<string>>) => {
+        event.preventDefault();
+        setState(event.currentTarget.value);
+    }
+
+    const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(`USERNAME:  ${username}`);
+        console.log(`PASSWORD:  ${password}`);
+        console.log(`PASSWORD MATCH:  ${passwordMatch}`);
+    }
     return (
         <div className='SignUp'>
             <h1>sign up</h1>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <label htmlFor="username">username:</label>
                 <input
                     name="username"
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleChange(event, setUsername)}
                     required
                     type="text"
                     value={username || ""}
@@ -19,19 +31,20 @@ function SignUpPage() {
                 <label htmlFor="password">password:</label>
                 <input
                     name="password"
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleChange(event, setPassword)}
                     required
-                    type="text"
+                    type="password"
                     value={password || ""}
                 />
                 <label htmlFor="passwordMatch">match password:</label>
                 <input
                     name="passwordMatch"
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleChange(event, setPasswordMatch)}
                     required
-                    type="text"
+                    type="password"
                     value={passwordMatch || ""}
                 />
+                <button type="submit">sign up</button>
             </form>
         </div>
     );
