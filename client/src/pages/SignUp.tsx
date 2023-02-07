64d70a0b48dc100e9441bd9b822a00773b424d8d
Jesse <sandvikjesse@gmail.com>
2023-02-06 21:13:49 -0500

    Client: US01, created username, password, and password match fields

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SignUpPage() {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordMatch, setPasswordMatch] = useState<string>();
     return (
         <div className='SignUp'>
-            <h1>SIGN UP PAGE</h1>
+            <h1>sign up</h1>
+            <form>
+                <label htmlFor="username">username:</label>
+                <input
+                    name="username"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    required
+                    type="text"
+                    value={username || ""}
+                />
+                <label htmlFor="password">password:</label>
+                <input
+                    name="password"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    required
+                    type="text"
+                    value={password || ""}
+                />
+                <label htmlFor="passwordMatch">match password:</label>
+                <input
+                    name="passwordMatch"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.currentTarget.value)}
+                    required
+                    type="text"
+                    value={passwordMatch || ""}
+                />
+            </form>
         </div>
     );
 }
