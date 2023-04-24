abd5eccb0d0a328d100c39ce7a8307cfaadad1a4
Jesse <sandvikjesse@gmail.com>
2023-04-24 08:06:32 -0400

    Client: create context wrapper for user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, createContext } from "react";
 import { NavLink, Routes, Route, useLocation } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -7,43 +8,59 @@ import SignUpPage from "./pages/signup/SignUp";
 
 import "./App.css";
 
+interface User {
+  email: String;
+  password: String;
+}
+const UserContext = createContext<User | undefined>(undefined);
+
+/**
+ * TODO: Light/Dark Theme toggle
+ */
+// type ThemeContextType = "light" | "dark";
+// const ThemeContext = createContext<ThemeContextType>("light");
+
 function App() {
   const location = useLocation();
 
+  const [user, setUser] = useState<User>();
+
   return (
-    <div className="App">
-      <header>
-        <nav className="navbar">
-          <div className="navbar-left-outer">
-            <NavLink className="navbar brand" to="/">
-              <i className="fa-solid fa-layer-group"></i>akago
-            </NavLink>
+    <UserContext.Provider value={user}>
+      <div className="App">
+        <header>
+          <nav className="navbar">
+            <div className="navbar-left-outer">
+              <NavLink className="navbar brand" to="/">
+                <i className="fa-solid fa-layer-group"></i>akago
+              </NavLink>
+            </div>
+          </nav>
+          <div className="navbar-right-outer">
+            {location.pathname !== "/login" && (
+              <NavLink to="/login">login</NavLink>
+            )}
+            {location.pathname !== "/signup" && (
+              <NavLink to="/signup">sign up</NavLink>
+            )}
           </div>
-        </nav>
-        <div className="navbar-right-outer">
-          {location.pathname !== "/login" && (
-            <NavLink to="/login">login</NavLink>
-          )}
-          {location.pathname !== "/signup" && (
-            <NavLink to="/signup">sign up</NavLink>
-          )}
-        </div>
-      </header>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-      <footer>
-        <div className="copyright">
-          <small>
-            Copyright &copy; {new Date().getFullYear()} Jesse Sandvik. All
-            rights reserved.
-          </small>
-        </div>
-      </footer>
-    </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <footer>
+          <div className="copyright">
+            <small>
+              Copyright &copy; {new Date().getFullYear()} Jesse Sandvik. All
+              rights reserved.
+            </small>
+          </div>
+        </footer>
+      </div>
+    </UserContext.Provider>
   );
 }
 
