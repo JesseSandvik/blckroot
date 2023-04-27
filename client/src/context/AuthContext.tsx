61e49ea1234add7523bb80b2352f3344720ea827
Jesse <sandvikjesse@gmail.com>
2023-04-26 21:05:18 -0400

    Client: using useLocalStorage hook to store user to local storage

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type Props = {
   children: ReactNode;
@@ -22,10 +23,11 @@ export const AuthContext = createContext<AuthContextType>({
 
 export const AuthProvider = (props: Props) => {
   const [user, setUser] = useState<User | null>(null);
+  const { setItem } = useLocalStorage();
 
   useEffect(() => {
-    console.log({ user });
-  }, [user]);
+    user && setItem("user", JSON.stringify(user));
+  }, [setItem, user]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 import "./Dashboard.css";
 
 function Dashboard() {
+  const { user } = useContext(AuthContext);
+  const { getItem } = useLocalStorage();
+
+  useEffect(() => {
+    console.log({ user });
+    console.log("LOCAL STORAGE: ", getItem("user"));
+  }, [getItem, user]);
+
   return (
     <main className="Dashboard">
       <section className="sidebar-menu">
