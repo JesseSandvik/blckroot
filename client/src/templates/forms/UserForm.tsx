ccbf24c3038f5b2c7366d2f3547e75ab2f39e983
Jesse <sandvikjesse@gmail.com>
2023-05-08 22:28:59 -0400

    user form template work

--- client/src/templates/forms/UserForm.tsx
+++ client/src/templates/forms/UserForm.tsx
@@ -1,29 +1,40 @@
-import { FormEventHandler } from "react";
+import { FormEventHandler, ReactNode } from "react";
 
 import Button from "../../atoms/button/Button";
 import Form from "../../organisms/form/Form";
 import Heading from "../../atoms/heading/Heading";
-import Icon from "../../atoms/icon/Icon";
-import LabeledInput from "../../molecules/labeledInput/LabeledInput";
 
 type UserFormTemplateProps = {
+  firstInput: ReactNode;
+  secondInput: ReactNode;
+  thirdInput?: ReactNode;
   onFormSubmit: FormEventHandler<HTMLFormElement>;
   pageClassName: string;
   pageHeading: string;
   submitButtonName: string;
+  submitButtonDisabled: boolean;
 };
 
 const UserFormTemplate = ({
+  firstInput,
+  secondInput,
+  thirdInput,
   onFormSubmit,
   pageClassName,
   pageHeading,
   submitButtonName,
+  submitButtonDisabled,
 }: UserFormTemplateProps): JSX.Element => {
   return (
     <main className={pageClassName}>
       <Heading tag="1">{pageHeading}</Heading>
       <Form onSubmit={onFormSubmit}>
-        <Button type="submit">{submitButtonName}</Button>
+        <div className="input-container">{firstInput}</div>
+        <div className="input-container">{secondInput}</div>
+        {thirdInput && <div className="input-container">{thirdInput}</div>}
+        <Button disabled={submitButtonDisabled} type="submit">
+          {submitButtonName}
+        </Button>
       </Form>
     </main>
   );
