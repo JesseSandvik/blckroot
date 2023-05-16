5713c55cae87804be224298e0c32cf962c092cd8
Jesse <sandvikjesse@gmail.com>
2023-05-16 05:44:44 -0400

    refactoring UserCredentialForm

--- client/src/organisms/form/UserCredentialForm.tsx
+++ client/src/organisms/form/UserCredentialForm.tsx
@@ -1,11 +1,44 @@
+import { FormEventHandler, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 import Button from "../../atoms/button/Button";
 import EmailInput from "../../molecules/labeledInput/EmailInput";
 import Form from "./Form";
 import PasswordConfirmInput from "../../molecules/labeledInput/PasswordConfirmInput";
 import PasswordInput from "../../molecules/labeledInput/PasswordInput";
 
-const UserCredentialForm = () => {
-  return <div>UserCredentialForm</div>;
+type UserCredentialFormTypes = {
+  onSubmit: FormEventHandler<HTMLFormElement>;
+};
+
+const UserCredentialForm = ({
+  onSubmit,
+}: UserCredentialFormTypes): JSX.Element => {
+  const location = useLocation();
+  const [buttonName, setButtonName] = useState<string>("");
+  const [showPasswordConfirmField, setShowPasswordConfirmField] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    location.pathname === "/signup" && setShowPasswordConfirmField(true);
+  }, [location]);
+
+  useEffect(() => {
+    location.pathname === "/signup"
+      ? setButtonName("create account")
+      : setButtonName("continue");
+  }, [location]);
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <EmailInput />
+      <PasswordInput />
+      {showPasswordConfirmField && <PasswordConfirmInput />}
+      <div>
+        <Button type="submit">{buttonName}</Button>
+      </div>
+    </Form>
+  );
 };
 
 export default UserCredentialForm;
